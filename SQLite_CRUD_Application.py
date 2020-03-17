3f1b3c5a6bb6e4f5a76c05e7c22b472b5e1c9031
Jin Jung <47117122+jijung1@users.noreply.github.com>
2020-03-17 13:00:05 -0700

    Options 1 and 2 working

--- SQLite_CRUD_Application.py
+++ SQLite_CRUD_Application.py
@@ -6,10 +6,15 @@ from Student import Student
 
 #studentID, firstName, lastName, gpa, major, facAdvisor, isDeleted
 
-conn = sqlite3.connect('C:\\Users\\jinju\\Documents\\CPSC_COURSES2\\CPSC_COURSES\\StudentDB')
+conn = sqlite3.connect('StudentDB.db')
 c = conn.cursor() #allows to python code to execute SQL statements
-#c.execute("DELETE FROM Student WHERE (FirstName = 'Covid')")
-#stu = Student(2329400, 'Jin', 'Jung', 3.96, 'CS', 'Rene German', False)
+
+c.executescript("""
+    CREATE TABLE IF NOT EXISTS Student ( StudentId INT PRIMARY KEY, FirstName VARCHAR(32), LastName VARCHAR(32), GPA FLOAT, Major VARCHAR(16), FacultyAdvisor VARCHAR(32), IsDeleted BOOLEAN );
+    """)
+
+c.execute ("CREATE TABLE IF NOT EXISTS Student ( StudentId INT PRIMARY KEY, FirstName VARCHAR(32), LastName VARCHAR(32), "
+ "GPA FLOAT, Major VARCHAR(16), FacultyAdvisor VARCHAR(32), IsDeleted BOOLEAN );")
 
 print('please select from the following choices:\n1. Display All Student Info\n2. Add New Student\n3. Update Advisor/Major\n4. Delete by Student ID\n5. Query by Major/GPA/Advisor\n6. exit')
 choice = int(input())
@@ -18,10 +23,14 @@ while choice != 6:
 
     #Display all student info
     if choice == 1:
-        c.execute('SELECT * FROM Student')
+        c.execute('SELECT "StudentId", "FirstName", "LastName", "GPA", "Major", "FacultyAdvisor" FROM Student')
         all_rows = c.fetchall()
-        df = DataFrame(all_rows, columns=["StudentId", "FirstName","LastName", "GPA", "Major", "FacultyAdvisor","IsDeleted"])
-        print(df)
+        df = DataFrame(all_rows, columns=["StudentId", "FirstName", "LastName", "GPA", "Major", "FacultyAdvisor"])
+        if df.empty:
+            print('Table is empty.')
+        else:
+           print(df)
+
     #Add new student with input validation (basic type check and add better during test phase)
     elif choice == 2:
         print('All information MUST be entered for record to be saved.')
@@ -35,13 +44,13 @@ while choice != 6:
         while True:
             print('Please enter First Name: ')
             firstname = input()
-            if firstname.isalpha():
+            if type(firstname) == str:
                 break
             print('You have entered an invalid First Name.')
         while True:
             print('Please enter Last Name: ')
             lastname = input()
-            if lastname.isalpha():
+            if type(lastname) == str:
                 break
             print('You have entered an invalid Last Name.')
         while True:
@@ -54,24 +63,55 @@ while choice != 6:
         while True:
             print('Please enter Major:')
             major = input()
-            if major.isalpha():     #maybe add checks from a list of valid majors
+            if type(major) == str:     #maybe add checks from a list of valid majors
                 break
             print('You have entered an invalid Major')
         while True:
             print('Please enter Student\'s Faculty Advisor:')
             facadvisor = input()
-            if facadvisor.isalpha():
+            if type(facadvisor) == str:
                 break
             print('You have entered an invalid Faculty Advisor.')
         stu = Student(studentid, firstname, lastname, gpa, major, facadvisor, False)
         c.execute ('INSERT INTO Student("StudentId", "FirstName","LastName", "GPA", "Major", "FacultyAdvisor", IsDeleted) VALUES (?,?,?,?,?,?,?)', stu.getStudent())
-
-        #print(type(studentid))
-        #print(studentid)
+        rowID = c.lastrowid
+        print("record created", rowID)
 
     elif choice == 3:
-        print('hello')
+        count = 0 #counter to time out if student id not found too many times
+        while True:
+
+            print('Please enter the Student ID of student to change Major/Advisor: ')
+            try:
+                student_id = int(input())
+                stmt = "SELECT StudentId FROM Student WHERE StudentId = " + str(student_id)
+                print("stmt: " + stmt)
+                c.execute(stmt)
+                r = c.fetchone()
+                if type(r) == tuple:
+                    break
+                else:
+                    count += 1
+                    print('Student ID not in Database.')
+                    if count == 3:
+                        break
+            except:
+                print('You have entered an invalid Student ID.')
+        if count == 3:
+            break;
+        print ('Please enter Student\'s new major: ')
+        major = input()
+
+
+
+
+
+
+
+       # c.execute("UPDATE Student SET Major = ? WHERE StudentId = ?", ('CS', 11,))
     elif choice == 4:
+        # c.execute("DELETE FROM Student WHERE (FirstName = 'Covid')")
+
         print('hello')
 
     elif choice == 5:
@@ -84,32 +124,19 @@ while choice != 6:
         print("invalid input!")
     print('please select from the following choices:\n1. Display All Student Info\n2. Add New Student\n3. Update Advisor/Major\n4. Delete by Student ID\n5. Query by Major/GPA/Advisor\n6. exit')
     choice = int(input())
-"""
-prompt user with switch options
-do either add record, (prompt Firstname, LastName, Major)
- delete records, (delete based on Firstname, Lastname) 
-  delete all records, (delete from table)
-   update attributes, ( query records by: first name, major, last name match
 
-"""
-
-#c.execute ('INSERT INTO Student("StudentId", "FirstName","LastName", "GPA", "Major", "FacultyAdvisor", IsDeleted) VALUES (?,?,?,?,?,?,?)', stu.getStudent())
-#c.execute("INSERT INTO Student('FirstName', 'LastName')" "VALUES ('Covid', 'German')")
 
 
 #update example
 """
-c.execute("UPDATE Student SET Major = ? WHERE StudentId = ?"
-          , ('CS', 11,)
-          )
+c.execute("UPDATE Student SET Major = ? WHERE StudentId = ?", ('CS', 11,))
 """
 """
 #select
 input_param = ('Rene', 'CS',)
 c.execute('SELECT FirstName, LastName, Major FROM Student WHERE FirstName = ? AND Major = ?', input_param)
 all_rows = c.fetchall()
-df = DataFrame(all_rows, columns=['StudentId', 'LastName', 'LastName', 'Major'])
-print(df)
+
 
 
 studentId = c.lastrowid
